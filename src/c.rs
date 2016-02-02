ac790090e45ee8702b636fe46451e0f23eaad183
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-02-02 09:36:01 +0100

    Update the examples [ci skip]

--- src/c.rs
+++ src/c.rs
@@ -9,7 +9,8 @@
 //! let mut a = vec![3.0, 1.0, 1.0, 1.0, 3.0, 1.0, 1.0, 1.0, 3.0];
 //! let mut w = vec![0.0; n as usize];
 //!
-//! dsyev(Layout::ColumnMajor, b'V', b'U', n, &mut a, n, &mut w);
+//! let info = dsyev(Layout::ColumnMajor, b'V', b'U', n, &mut a, n, &mut w);
+//! assert_eq!(info, 0);
 //!
 //! for (one, another) in w.iter().zip(&[2.0, 2.0, 5.0]) {
 //!     assert!((one - another).abs() < 1e-14);
